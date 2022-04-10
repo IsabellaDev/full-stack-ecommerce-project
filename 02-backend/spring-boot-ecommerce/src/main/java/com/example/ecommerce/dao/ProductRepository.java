db4ed862de43ccd7bbf12ad39835393db21a7833
Isabella Liu <isabellaliu77@gmail.com>
2022-04-10 00:47:26 -0400

    added basic front-end stuff

--- 02-backend/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
@@ -3,7 +3,9 @@ package com.example.ecommerce.dao;
 
 import com.example.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
