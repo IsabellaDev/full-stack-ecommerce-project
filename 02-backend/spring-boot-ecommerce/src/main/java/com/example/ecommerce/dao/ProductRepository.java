665120eba5d5e370ad09f4cc7a3c9000bda23fb1
Isabella Liu <isabellaliu77@gmail.com>
2022-04-11 21:41:26 -0400

    added hard-coded searched by category feature

--- 02-backend/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
@@ -2,11 +2,16 @@ package com.example.ecommerce.dao;
 
 
 import com.example.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin(origins = "http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    // spring generate corresponding select statement behind
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
 }
--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -10,15 +11,29 @@ import { Product } from 'src/app/common/product';
 export class ProductListComponent implements OnInit {
 
   products!: Product[];
+  currentCategoryId!: number;
   
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, 
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+
+    // check id valid or not 
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the id, convert from string to number by Number()
+      this.currentCategoryId = Number(this.route.snapshot.paramMap.get('id'));
+    } else {
+      this.currentCategoryId = 1;
+    }
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
