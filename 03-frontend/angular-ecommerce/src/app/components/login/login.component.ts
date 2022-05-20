8dc0a91913d8f1230180cd0735c1c2966281ec66
Isabella Liu <isabellaliu77@gmail.com>
2022-05-20 12:25:44 -0400

    added okta sign-in-widget

---
+++ 03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { OKTA_AUTH, OKTA_CONFIG, OktaAuthModule } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+
+import OktaSignIn from '@okta/okta-signin-widget';
+
+import myAppConfig from '../../config/my-app-config';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  oktaSignIn: any;
+  constructor(@Inject(OKTA_AUTH) private oktaAuthService: OktaAuth) {
+
+    this.oktaSignIn = new OktaSignIn({
+      logo: 'assets/images/logo.PNG',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.oktaSignIn.remove();
+
+    this.oktaSignIn.renderEl({
+      el: '#okta-sign-in-widget'
+    }, // name should be same as div tag id in login.component.html
+      (response) => {
+        if (response.status === 'SUCCESS') {
+          this.oktaAuthService.signInWithRedirect();
+        }
+      },
+      (error) => {
+        throw error;
+      }
+    );
+  }
+
+}
---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa52jjkrwu7YBgSI5d7', 
+        issuer: 'https://dev-95645334.okta.com/oauth2/default', 
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
