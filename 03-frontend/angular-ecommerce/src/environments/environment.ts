fe2cfbdfd211ec2f483fd4a1b25e8ec5c1ed22dd
Isabella Liu <isabellaliu77@gmail.com>
2023-05-16 12:58:12 -0400

    Added payment function using Stripe

--- 03-frontend/angular-ecommerce/src/environments/environment.ts
+++ 03-frontend/angular-ecommerce/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  luv2shopApiUrl: "https://localhost:8443/api"
+  luv2shopApiUrl: "https://localhost:8443/api",
+  stripePublishableKey: "pk_test_51N74GUAVhVXlcUjUI3W2hNzMhaq64MfF3HSvnCZT6YUmRd4FdFLsmXhRQ6PjKs6Q4XLQi9WQ6MlZklH1jzdhB3iQ00hoTfVM59"
 };
 
 /*
