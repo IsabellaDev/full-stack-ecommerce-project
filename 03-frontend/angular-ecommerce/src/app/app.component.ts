f14379b94dd000a6a11bc0f151e7d9fce316b5f9
Isabella Liu <isabellaliu77@gmail.com>
2023-05-24 02:01:18 -0400

    updated some layout

--- 03-frontend/angular-ecommerce/src/app/app.component.ts
+++ 03-frontend/angular-ecommerce/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ecommerce';
+
+  gfgMenu() {
+    const GFG = document.querySelector('.hamburger-inner');
+    if (GFG.classList.contains('d-none')) {
+        GFG.classList.remove('d-none');
+        GFG.classList.add('d-block');
+    }
+    else {
+        GFG.classList.add('d-none');
+    }
+  }
+
 }
